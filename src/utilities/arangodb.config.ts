16a7f6310832e6d750e3b17ca2e2d1475af86bf8
Shinta56 <72849970+Shinta56-tech@users.noreply.github.com>
2024-10-27 08:49:53 +0000

    start utilities/arangodb

--- src/utilities/arangodb.config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  // ArangoDB設定
-  url: 'http://localhost:8529',
-  databaseName: 'NetkeibaDBML',
-  username: 'root',
-  password: 'password',
-};
-
-export default config;
--- src/utilities/arangodb.ts
+++ src/utilities/arangodb.ts
@@ -1,19 +1,48 @@
 import { Database } from 'arangojs';
 import { QueryOptions } from 'arangojs/database';
-import config from './arangodb.config';
 
-const db = new Database({
-  url: config.url,
-  databaseName: config.databaseName,
+export interface ArangoDBConfig {
+  url: string;
+  databaseName: string;
   auth: {
-    username: config.username,
-    password: config.password,
-  },
-});
+    username: string;
+    password: string;
+  };
+}
 
-export const query = async (aql: string, options?: QueryOptions) => {
+export interface NeuronSchema {
+  name: string;
+  field: string;
+  num: number;
+  i1: number | string;
+  i2?: number | string;
+  i3?: number | string;
+}
+
+let db: any; // Database
+
+export const connect = (config?: ArangoDBConfig | undefined): void => {
+  if (!config) {
+    db = new Database({
+      url: 'http://localhost:8529',
+      databaseName: 'NetkeibaDBML',
+      auth: {
+        username: 'root',
+        password: 'password',
+      },
+    });
+  } else {
+    db = new Database(config);
+  }
+};
+
+export type QueryResult = number[] | NeuronSchema[];
+
+// prettier-ignore
+export const query = async (aql: string, options?: QueryOptions): Promise<QueryResult> => {
   const cursor = await db.query(aql, options);
-  return await cursor.all();
+  const result = await cursor.all();
+  return result;
 };
 
 export { QueryOptions };
